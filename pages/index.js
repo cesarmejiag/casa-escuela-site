959c6e4d917cd154c06d06aff5b6b92a966fb272
César Mejía <iconfidence@gmail.com>
2022-04-08 19:34:05 -0500

    Create home to section

--- pages/index.js
+++ pages/index.js
@@ -23,30 +23,26 @@ export default function Home() {
         </div>
       </Section>
       <Section id="home-to" intro={homeTo.intro}>
-        <div className="row">
-          <div className="col-6">
-            <div className="home-to-image-1">
-              <Image
-                src={homeTo.imageSrc1}
-                layout="responsive"
-                width={1050}
-                height={935}
-              />
+        <div className="home-to-wrapper">
+          <div className="row">
+            <div className="col-12 col-lg-8">
+              <div className="home-to-image-1">
+                <ImageSwicher
+                  imagesSrc={[homeTo.imageSrc1, homeTo.imageSrc1]}
+                />
+              </div>
+              <div className="home-to-image-2">
+                <ImageSwicher
+                  imagesSrc={[homeTo.imageSrc2, homeTo.imageSrc2]}
+                />
+              </div>
             </div>
-            <div className="home-to-image-2">
-              <Image
-                src={homeTo.imageSrc1}
-                layout="responsive"
-                width={1020}
-                height={975}
-              />
+            <div className="col-12 col-lg-4">
+              <div
+                className="home-to-text"
+                dangerouslySetInnerHTML={{ __html: homeTo.text }}
+              ></div>
             </div>
-          </div>
-          <div className="col-6">
-            <div
-              className="home-to-text"
-              dangerouslySetInnerHTML={{ __html: homeTo.text }}
-            ></div>
           </div>
         </div>
       </Section>
@@ -56,6 +52,49 @@ export default function Home() {
         .what-we-do-image {
           padding-bottom: 46%;
           position: relative;
+        }
+
+        .home-to-wrapper {
+          margin-top: 80px;
+        }
+
+        .home-to-image-1 {
+          padding-bottom: 80.252%;
+          position: relative;
+          width: 90.207%;
+        }
+
+        .home-to-image-2 {
+          left: 15%;
+          margin-top: -12%;
+          padding-bottom: 80.494%;
+          position: relative;
+          width: 85%;
+        }
+
+        .home-to-text {
+          margin-top: 60px;
+          max-width: 350px;
+        }
+
+        @media screen and (min-width: 768px) {
+          .home-to-wrapper {
+            margin-top: 100px;
+          }
+        }
+
+        @media screen and (min-width: 992px) {
+          .home-to-image-1 {
+            padding-bottom: 80.252%;
+            width: 90.207%;
+          }
+  
+          .home-to-image-2 {
+            left: 45%;
+            margin-top: -25%;
+            padding-bottom: 83.764%;
+            width: 87.63%;
+          }
         }
       `}</style>
     </Layout>
