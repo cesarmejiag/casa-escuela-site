59a76c92e34eac1928967f7638e1ef26e50cea9e
César Mejía <iconfidence@gmail.com>
2022-04-09 10:17:11 -0500

    Add favicons

--- components/Layout.js
+++ components/Layout.js
@@ -13,6 +13,9 @@ const Layout = ({ children, pageTitle }) => {
       <ParallaxProvider>
         <Head>
           <title>{title}</title>
+          <link rel="icon" href="/favicons/favicon.ico" type="image/x-icon" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png" />
         </Head>
         <Header />
         <div className="outer-wrapper">{children}</div>
