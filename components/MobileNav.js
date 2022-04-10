fc1e5c88e3185b553410218e502b336849c2cc54
César Mejía <iconfidence@gmail.com>
2022-04-09 21:22:58 -0500

    Create MobileNav component.

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,7 +1,50 @@
-import React from "react";
+import styles from "../styles/MobileNav.module.css";
 
-const MobileNav = ({visible}) => {
-  return <div style={{ display: visible ? 'block' : 'none' }}>MobileNav</div>;
+import Link from "next/link";
+import Image from "next/image";
+
+const MobileNav = ({ visible }) => {
+  const options = [
+    { path: "/be-our-guest", label: "Be our guest" },
+    { path: "/happenings", label: "Happenings" },
+    { path: "/residencies", label: "Residencies" },
+    { path: "/about", label: "About" },
+    { path: "/contact", label: "Contact" },
+    { path: "#", label: "Shop" },
+  ];
+  const createLink = (path, text) => (
+    <Link href={path}>
+      <a className={styles.link}>{text}</a>
+    </Link>
+  );
+
+  return (
+    <div className={`${styles.mobileNav} ${visible && styles.expanded}`}>
+      <div className="container-fluid">
+        <div className={styles.inner}>
+          <ul class={styles.list}>
+            {options.map(({ path, label }) => (
+              <li>{createLink(path, label)}</li>
+            ))}
+          </ul>
+          <div className={styles.bottom}>
+            <div>
+              <Image
+                src="/images/icon-ce.svg"
+                width={32}
+                height={21}
+                priority
+              />
+            </div>
+            <div className={styles.text}>
+              Your sanctuary
+              <br /> in the heart of Merida
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MobileNav;
