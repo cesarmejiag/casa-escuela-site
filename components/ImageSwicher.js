764c4beeb2fde761cb423c7682bf0c6220936aa1
César Mejía <iconfidence@gmail.com>
2022-04-13 13:49:47 -0500

    Fix styles 2

--- components/ImageSwicher.js
+++ components/ImageSwicher.js
@@ -5,8 +5,10 @@ import { useParallax } from "react-scroll-parallax";
 import PropTypes from "prop-types";
 import Image from "next/image";
 import useCounter from "../hooks/useCounter";
+import useResize from "../hooks/useResize";
 
 const ImageSwicher = ({ imagesSrc, textPosition, parallaxSpeed }) => {
+  const { mobile } = useResize();
   const total = imagesSrc.length - 1;
   const { count, increase } = useCounter(0, total, true);
   const parallax = useParallax({
