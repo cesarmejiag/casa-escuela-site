5364aa0fd532f516575ceb15beeb76e7f29d0d2b
César Mejía <iconfidence@gmail.com>
2022-04-22 12:07:39 -0500

    Start with sanity

---
+++ studio/schemas/pages/home.js
@@ -0,0 +1,15 @@
+export default {
+  name: "home",
+  title: "Home",
+  type: "document",
+  fields: [
+    {
+      name: "mainImage",
+      title: "Main Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
