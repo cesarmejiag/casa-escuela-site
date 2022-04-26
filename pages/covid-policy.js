a1d5722f6b008e8e7b2502ca7ee3c261471ccd74
César Mejía <iconfidence@gmail.com>
2022-04-25 21:44:54 -0500

    Remove covid policy text and remove hover in nav options

--- pages/covid-policy.js
+++ pages/covid-policy.js
@@ -4,48 +4,7 @@ import Section from "../components/Section";
 const covidPolicy = () => {
   return (
     <Layout pageTitle="Covid Policy">
-      <Section id="covid-policy">
-        <div className="covid-title">
-          AT ALMA MAYA WE TAKE EVERY POSSIBLE MEASURE TO MAKE YOUR STAY IS AS
-          COMFORTABLE AND SAFE AS POSSIBLE.
-        </div>
-        We have detailed all measures in place below. Please take note of them
-        before you arrive at Alma Maya Resort.
-        <div className="covid-title">Before your arrival</div>
-        Remember to check-in online before your stay to limit contact. You can
-        always email, WhatsApp or call us prior to your arrival in case you have
-        questions or need assistance checking in. You will receive an email with
-        your confirmation details after booking. This email will include your
-        check-in information. Don&#39;t forget to add a copy/picture of your ID
-        in the profile tab. Your arrival at Alma Maya Resort. Arrival at the
-        large parking lot in front of the reception You carry your own luggage
-        to the reception Lobby. Welcome at the reception desk Please wait
-        outside the chalet. A staff member at the reception desk will invite you
-        to enter once the other guests have left to explain how the hotel works.
-        <div className="covid-title">Discovery of your room</div>
-        You can walk up to your room on your own with a location map or you can
-        be accompanied by one of our staff depending on how busy the hotel is.
-        If you need assistance with your luggage, please contact one of the
-        staff members upon arrival. Your room is cleaned and disinfected before
-        and after your stay. Breakfast, picnic, lunch, tea-time, dinner meals
-        can be delivered directly to the entrance of your room, fully
-        contactless. Meals are ordered at the reception desk at the time of
-        check-in, by WhatsApp or our Portal.
-        <div className="covid-title">Care & Meditation Center</div>
-        The relaxation area is only accessible if you have a reserved treatment.
-        Thank you for coming at the right time. The meditation center is
-        accessible to all free of charge by prior reservation, in 30-minute
-        increments, at the reception desk. The meditation center is open from 8
-        am to 8 pm.
-        <div className="covid-title">Departure</div>
-        The same rules apply as when you arrived: if there are guests inside,
-        please wait outside.
-        <div className="covid-title">Covid- 19 Guidelines</div>
-        Masks required on premises at all times. Social distancing protocols are
-        required and enforced by staff and security. Bubbles and domes are
-        disinfected between guests. Please contact us with any questions you may
-        have.
-      </Section>
+      <Section id="covid-policy"></Section>
     </Layout>
   );
 };
